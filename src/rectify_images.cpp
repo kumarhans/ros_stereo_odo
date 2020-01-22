6b791b87fb676556ec8b5a91a88ef18155aeb572
Hans Kumar <hansk@andrew.cmu.edu>
2020-01-22 16:21:48 -0500

    rectify images started writing

---
+++ src/rectify_images.cpp
@@ -0,0 +1,24 @@
+
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+
+#include <iostream>
+#include <ctype.h>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <ctime>
+#include <sstream>
+#include <fstream>
+#include <string>
+ 
+
+
+
+
+//stereoRectify
+//initUndistortRectifyMap
+//remap
